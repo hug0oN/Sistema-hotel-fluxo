4186a5a3bb74b11a7df15dcbc5bf2644cbe60ac1
hug0oN <hugoestradaalves@gmail.com>
2026-09-12 11:06:31 -0300

    Estrutura das classes com metodos + prompt

--- sistema.js
+++ sistema.js
@@ -1,14 +1,63 @@
-const 
-
-
+const prompt = require('prompt-sync')(); //esse aqui eh o melhor jeito que eu achei para fazer input
+//talvez precise instalar o prompt sync antes
 
 class sistema{
+    constructor(){ //construtor so pra uma variavel eh sacanagem
+        this.dev_mode = false;
+    }
 
-
+    ativar_dev(){ //ativa o dev mode, que vai habilitar varias mensagens de teste
+        this.dev_mode = true;
+        if(Sistema.dev_mode == true){
+            console.log("DEV: Modo dev ativado");
+        }
+    }
+    
     login(){
+
+        if(Sistema.dev_mod == true){
+        console.log("DEV: Iniciando processo de login");
+        }
+        console.log("Você é funcionário ou cliente?");
+        //aqui pesquisar alguma maneira de criar uma janela de input e multipla escolha 
+        //ao inves de digitacao
+        //se nao der eu uso numeros (opcao 1 2 3 etc)
+
+        nome_login = prompt("Por favor digite seu nome de usuário: ");
+
+
+        senha_login = prompt("Agora digite sua senha: ");
+
 
     }
     cadastro(){
+        tipo_cadastro;
+        if(Sistema.dev_mod == true){
+        console.log("DEV: Iniciando processo de cadastro");
+        }
+        //Escolher entre funcionario e cliente
+
+        tipo_cadastro = 1;
+        
+        tipo_cadastro = 2;
+
+        //receber inputs dos dados
+
+        //fazer o id unico
+
+        //chamar construtor
+
+        //ir para o menu de cliente/funcionario
+    }
+
+    menu_cliente(){
+
+
+    }
+
+
+    menu_funcionario(){
+
 
     }
 
@@ -19,28 +68,46 @@ class sistema{
 }
 const Sistema = new sistema;
 
-/* Um copia e cola de texto pra testes 
-if(Sistema.dev_mode == "true"){
+/* Um copia e cola de texto para testes 
+if(Sistema.dev_mode == true){
     console.log("")
 }
 */
 
+
+//aqui algumas funcoes que eu deixei como globais ja que ambos clientes e funcionarios
+//acessam, usando os id unicos para identificar
+
+
 function mudar_senha(identificador){
-        
-    }
+    //algum jeito de deixar como asteristico quando digitar?
+    
+}
 
 function ver_dados(identificador){
 
 }
 
+function ver_avaliacao(){
+    //aqui puxar todas as avaliacoes em forma de lista para escolher qual visualizar
+
+}
+
+function ver_lista_quartos(){
+    //puxar todos os quartos em uma lista
+
+}
+
+
 class reserva{
-    constructor(cliente, status, entrada, saida){
+    constructor(cliente, entrada, saida){
         this.cliente = cliente;
-        this.status = status;
+        this.status = "Normal";
         this.entrada = entrada;
         this.saida = saida;
         //parte para a construcao do id unico
-
+        //id unicos sao compartilhados entre funcionarios e clientes
+        //o q vai facilitar pra chamar funcoes globais
 
     }
     cancelar(){
@@ -51,6 +118,9 @@ class reserva{
 
         this.status = "Concluída."
     }
+    avaliar_estadia(){
+
+    }
 
 
 }
@@ -58,14 +128,13 @@ class reserva{
 class funcionario{
     #senha; //como eu vou definir senha como um atributo privado
             //pra proteger, eu uso esse # pra deixar privado (documentaçao da internet)
-    constructor(nome,cpf,email,senha){
+    constructor(id,nome,cpf,email,senha){
         //construtor definindo os atributos basicos
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
         this.#senha = senha;
-        //aqui entra a parte de id unico
-
+        this.id = id;
     }
     mudar_reserva(){
 
@@ -82,21 +151,26 @@ class funcionario{
     editar_quartos(){
 
     }
+    ver_lista_clientes(){
 
+    }
+    ver_lista_funcionarios(){
+
+    }
 
 }
 
 
 class cliente{
     #senha; //mesmo caso do funcionario
-    constructor(nome,cpf,data_nasc,email,senha){
+    constructor(id,nome,cpf,data_nasc,email,senha){
         //construtor definindo os atributos basicos
         this.nome = nome;
         this.cpf = cpf;
         this.data_nasc = data_nasc;
         this.email = email;
         this.#senha = senha;
-        //aqui entra a parte de id unico
+        this.id = id;
         
     }
     fazer_reserva(){
@@ -108,13 +182,9 @@ class cliente{
     ver_reservas(){
 
     }
-    mudar_dados(){
+    mudar_dados_cliente(){
 
     }
-    avaliar_estadia(){
-
-    }
-
 
 
 }
@@ -127,7 +197,9 @@ class avaliacao{
         this.descricao = descricao;
         this.data = data;
     }
+    exibir_avaliacao(){
 
+    }
 
 }
 
@@ -137,6 +209,9 @@ class quartos{
         this.preco = preco;
         this.nome = nome;
         this.descricao = descricao;
+    }
+    mudar_dados_quarto(){
+
     }
 
 
